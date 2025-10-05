d54b7d7747a960abd5665ee3f43f4a44d1801705
kamile2105 <c00312390@setu.ie>
2025-10-05 15:22:00 +0100

    Add files via upload

---
+++ lab2bq2.java/Rectangle.java
@@ -0,0 +1,63 @@
+
+public class Rectangle
+{
+    private int Length; // private attributes to store dimensions of the rectangle
+    private int Width;
+
+
+public Rectangle() // default constructor that initialises length and width to 1
+{
+    this.Length = 1;
+    this.Width = 1;
+}
+
+public void setLength( int Length ) // setter method for length with a validation
+{
+    if(Length > 0.0 && Length <= 40.0){
+         this.Length = Length;
+    }
+    else {
+        System.out.println("Invalid Length. Must be bigger than 0 & less than 40.");
+    }
+
+}
+
+public void setWidth( int Width) // setter here aswell but for width with a validation also
+{
+    if(Width > 0.0 && Width <= 40.0)
+    {
+        this.Width = Width;
+    }
+    else{
+        System.out.println("Invalid Width. Must be bigger than 0 & less than 40.");
+    }
+}
+
+public int getLength() // getter for length
+{
+    return Length;
+}
+
+public int getWidth() // getter for width
+{
+    return Width;
+}
+
+public int getArea() // area method 
+{
+    return Length * Width; // calculation
+}
+
+public int getPerimeter() // perimeter method 
+{
+    return 2 * (Length + Width); // calculation
+}
+
+public String toString() // tostring method to return a string representation of the rectangle object
+{
+    return  "Length = " + Length + " Width = " + Width;
+}
+
+
+
+}
---
+++ lab2bq2.java/RectangleDriver.java
@@ -0,0 +1,34 @@
+
+// Student Name     : Kamile Kacinskaite
+// Student Id Number: C00312390
+// Date             : 05/10/25
+// Purpose          : Lab2bq2
+
+public class RectangleDriver // driver program to test the rectangle class
+{
+    public static void main(String[] args) 
+    {
+       Rectangle Rect = new Rectangle(); // new rectangle object using default constructor
+
+        System.out.println("The Default Values: "); // display default values of length and width
+        System.out.println(Rect.toString());
+
+        Rect.setLength(5); // set valid values
+        Rect.setWidth(10);
+
+        System.out.println("Updated values: " + Rect.toString()); // display updated values
+        
+        // test with setting invalid values for length and width
+        Rect.setLength(-3); // invalid has to be less than zero
+        Rect.setWidth(43); // invalid also has to be greater than 40
+
+        System.out.println("After invalid attempts: " + Rect.toString()); // show values after attempting invalid values
+        // display individual values using getters
+        System.out.println("Length is: " + Rect.getLength());
+        System.out.println("Width is: " + Rect.getWidth());
+
+        System.out.println("Area: " + Rect.getArea()); // test and display the area
+
+        System.out.println("Perimeter: " + Rect.getPerimeter()); // test and display the perimeter 
+    }
+}
